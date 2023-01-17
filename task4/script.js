8f0e79d9845742e2216bd4002fa3a4d941dc10d8
GrGrGrRoman <grroman@live.com>
2023-01-17 11:57:36 +0300

    update fales task5

--- task4/script.js
+++ task4/script.js
@@ -1,10 +1,7 @@
 const textLink = document.querySelector('#link'); //находим нужный элемент
-textLink.addEventListener('click', function(event) {
-    const resultText = prompt('Введите текст ссылки'); //навешиваем обработчик на событие
+textLink.addEventListener('click', (event) => { //навешиваем обработчик на событие
+    const resultText = prompt('Введите текст ссылки'); //записываем в константу текст из диалогового окна
     console.log(resultText); //проверяем создание переменной с введеным текстом
     textLink.textContent = resultText; //заменяем текст ссылки
     event.preventDefault(); //предотвращаем поведение по умолчанию, т.е. переход по ссылке
 })
-
-
-
--- task5/script.js
+++ task5/script.js
@@ -1,1 +1,14 @@
-//поле формы
+const field = document.querySelector('#field'); //поле ввода
+const button = document.querySelector('#button'); //кнопка
+const duplicateField = document.querySelector('#duplicateField'); //текстовое поле
+
+field.addEventListener('keydown', (event) => {
+    //console.log(event.key);
+    duplicateField.textContent = field.value;
+    
+})
+
+button.addEventListener('click', (event) => {
+    console.log(duplicateField.textContent);
+    
+})
