17ca141f503e8f24acd8282491b9f754c9885fca
GrGrGrRoman <grroman@live.com>
2023-01-17 15:29:25 +0300

    update files

--- task5/script.js
+++ task5/script.js
@@ -2,15 +2,13 @@ const field = document.querySelector('#field'); //поле ввода
 const button = document.querySelector('#button'); //кнопка
 const duplicateField = document.querySelector('#duplicateField'); //текстовое поле
 
-field.addEventListener('keydown', (event) => {
-    //console.log(event.key);
-    duplicateField.textContent = field.value;
-    
-})
+field.addEventListener('input', () => { //обработчик на событие ввод текста
+    duplicateField.textContent = field.value; //присваиваем значение из поля ввода в текстовое поле
+    })
 
-button.addEventListener('click', (event) => {
-    console.log(duplicateField.textContent);
-    field.value = '';
-    duplicateField.textContent = '';
-    event.preventDefault();
+button.addEventListener('click', (event) => { //обработчик на событие нажатие на кнопку кнопкой мыши
+    event.preventDefault(); //предотвращение отправки формы
+    console.log(duplicateField.textContent); //вывод в консоль содержимого текстового поля
+    field.value = ''; //очистка поля ввода
+    duplicateField.textContent = ''; //очистка текстового поля
 })
