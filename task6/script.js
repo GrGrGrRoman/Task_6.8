3a07c5d924d106ffd21f56f90f09ac9b41b155ae
GrGrGrRoman <grroman@live.com>
2023-01-18 15:36:39 +0300

    update files

--- task6/script.js
+++ task6/script.js
@@ -1,27 +1,3 @@
-const trafficLightToggle = document.querySelector('#trafficLight');
-
-trafficLightToggle.addEventListener('click', makeGreen);
-
-function makeGreen() {
-    trafficLightToggle.style.background = ('green');
-    trafficLightToggle.removeEventListener('click', makeGreen);
-    console.log('зеленый');
-    trafficLightToggle.addEventListener('click', makeYellow);
-   }
-
-function makeYellow() {
-    trafficLightToggle.style.background = ('yellow');
-    trafficLightToggle.removeEventListener('click', makeYellow);
-    console.log('желтый');
-    trafficLightToggle.addEventListener('click', makeRed);    
-}
-
-function makeRed() {
-    trafficLightToggle.style.background = ('red');
-    trafficLightToggle.removeEventListener('click', makeRed);
-    console.log('красный');
-    trafficLightToggle.addEventListener('click', makeGreen);
-}
 //решение мне не нравится, но сделал так, как умею на данный момент
 const trafficLightRed = document.querySelector('#trafficLightRed');
 const trafficLightYellow = document.querySelector('#trafficLightYellow');
